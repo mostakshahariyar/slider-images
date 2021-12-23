7cf12f0c4369079e5ffead03ed04e33a8ca3dee5
Mostak-shahariyar <mostakshahariyar18@gmail.com>
2021-12-23 11:22:09 +0600

    final

--- js/slider.js
+++ js/slider.js
@@ -1,35 +1,27 @@
-const images = [
-   'images/pic-1.jpg',
-   'images/pic-2.jpg',
-   'images/pic-3.jpg',
-   'images/pic-4.jpg',
-   'images/pic-5.jpg',
-   'images/pic-6.jpg',
-   'images/pic-7.jpg',
-   'images/pic-8.jpg',
-   'images/pic-9.jpg',
-   'images/pic-10.jpg',
-   'images/pic-11.jpg',
-   'images/pic-12.jpg',
-   'images/pic-13.jpg',
-   'images/pic-14.jpg',
-   'images/pic-15.jpg',
-   'images/pic-16.jpg',
-   'images/pic-17.jpg',
-   'images/pic-18.jpg',
-   'images/pic-19.jpg',
-   'images/pic-20.jpg',
-   'images/pic-21.jpg',
-   'images/pic-22.jpg'
+const images =[
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg',
+    'images/pic-10.jpg',
+    'images/pic-11.jpg',
+    'images/pic-12.jpg'
 ]
 let imgIndex = 0;
-const imagesId = document.getElementById('image-slider')
+
+const imgary = document.getElementById('imgUrl');
 setInterval(() => {
-    imgIndex++;
     if (imgIndex>=images.length){
         imgIndex = 0;
     }
-    const imgUrl = images[imgIndex];
-    // console.log(imgUrl);
-    imagesId.setAttribute('src', imgUrl)
+    const imagesId = images[imgIndex];
+    imgIndex++;
+    imgary.setAttribute('src',imagesId)
+    console.log(imagesId);
+
 }, 2000);
